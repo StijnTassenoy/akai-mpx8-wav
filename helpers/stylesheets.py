e3f7704138128f6510ad6b26270a1e6b14faa089
StijnTassenoy <stijn.tassenoy@gmail.com>
2023-04-11 19:14:00 +0200

    Added edit pane and logo

---
+++ helpers/stylesheets.py
@@ -0,0 +1,21 @@
+right_pane_style = """
+    QListView {
+        font-size: 14px;
+        background-color: white;
+    }
+    
+    QListView::item {
+        padding: 3px;
+    }
+    
+    QListView::item:hover {
+        padding: 3px;
+        background-color: #f7cdcd;
+    }
+    QListView::item:selected {
+        padding: 3px;
+        background-color: #bd0000;
+        border: none;
+        color: white;
+    }
+"""
--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import os
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QStandardItem, QStandardItemModel
 from PyQt5.QtWidgets import QApplication, QMainWindow, QListView, QAbstractItemView, QLabel, QWidget, \
-    QVBoxLayout, QHBoxLayout
+    QVBoxLayout, QHBoxLayout, QFormLayout, QLineEdit
+from PyQt5 import QtGui
 
+from helpers.stylesheets import right_pane_style
 from models.sound_effect import SoundEffect
 
 
@@ -14,6 +16,7 @@ class MainWindow(QMainWindow):
         self.soundeffect_list = []
 
         self.setWindowTitle("AKAI MPX8 Manager")
+        self.setWindowIcon(QtGui.QIcon("images/logo.png"))
 
         # Create the left pane for drag and drop
         self.left_pane = QLabel("Drag and drop files here")
@@ -28,24 +31,8 @@ class MainWindow(QMainWindow):
         self.right_pane = QListView()
         self.right_pane.setSelectionMode(QAbstractItemView.SingleSelection)
         self.right_pane.setEditTriggers(QAbstractItemView.NoEditTriggers)
-        self.right_pane.setStyleSheet(
-            """
-            QListView {
-                font-size: 14px;
-                background-color: white;
-            }
-
-            QListView::item {
-                padding: 3px;
-            }
-
-            QListView::item:selected {
-                padding: 3px;
-                background-color: lightblue;
-                border: none;
-            }
-            """
-        )
+        self.right_pane.setStyleSheet(right_pane_style)
+        self.right_pane.clicked.connect(self.show_edit_pane)
 
         # Create a model for the right pane
         self.model = QStandardItemModel(self.right_pane)
@@ -58,13 +45,28 @@ class MainWindow(QMainWindow):
         pane_layout.addWidget(self.left_pane)
         pane_layout.addWidget(self.right_pane)
 
+        self.current_edit_pane = None
+
+        # Create a layout for the edit pane
+        edit_layout = QFormLayout()
+        self.output_name_edit = QLineEdit()
+        self.cut_from_edit = QLineEdit()
+        self.cut_to_edit = QLineEdit()
+        edit_layout.addRow(QLabel("Name:"), self.output_name_edit)
+        edit_layout.addRow(QLabel("Cut from:"), self.cut_from_edit)
+        edit_layout.addRow(QLabel("Cut to:"), self.cut_to_edit)
+        self.edit_pane = QWidget()
+        self.edit_pane.setLayout(edit_layout)
+        self.edit_pane.hide()
+
         # Create a layout for the main window
-        main_layout = QVBoxLayout()
-        main_layout.addLayout(pane_layout)
+        self.main_layout = QVBoxLayout()
+        self.main_layout.addLayout(pane_layout)
+        self.main_layout.addWidget(self.edit_pane)
 
         # Create a central widget and set the main layout
         central_widget = QWidget()
-        central_widget.setLayout(main_layout)
+        central_widget.setLayout(self.main_layout)
         self.setCentralWidget(central_widget)
 
         # Initialize the file list
@@ -79,7 +81,8 @@ class MainWindow(QMainWindow):
     def dropEvent(self, event):
         for url in event.mimeData().urls():
             path = url.toLocalFile()
-            if os.path.isfile(path) and path.endswith(".wav"):
+            extension = path.rsplit(".", 1)[1]
+            if os.path.isfile(path) and extension.lower() in ("wav", "mp3", "opus"):
                 if path not in self.file_list:
                     self.file_list.append(path)
                     item = QStandardItem(os.path.basename(path))
@@ -87,6 +90,43 @@ class MainWindow(QMainWindow):
                     soundeffect = SoundEffect(source_path=path, output_path="", output_name="")
                     self.soundeffect_list.append(soundeffect)
         print(self.soundeffect_list)
+
+    def show_edit_pane(self, index):
+        # Hide the currently shown edit pane, if any
+        if self.current_edit_pane is not None:
+            self.current_edit_pane.hide()
+
+        # Create a new edit pane
+        self.current_edit_pane = QWidget()
+        edit_layout = QFormLayout()
+
+        # Get the selected item's name
+        name = self.model.itemFromIndex(index).text()
+
+        # Create a label with the name
+        label_name = QLabel(name)
+        edit_layout.addRow("Name:", label_name)
+
+        # Create an input box for the output name
+        output_name_input = QLineEdit()
+        edit_layout.addRow("Output Name:", output_name_input)
+
+        # Create an input box for the starting time
+        start_time_input = QLineEdit()
+        edit_layout.addRow("Start Time:", start_time_input)
+
+        # Create an input box for the ending time
+        end_time_input = QLineEdit()
+        edit_layout.addRow("End Time:", end_time_input)
+
+        # Set the layout for the edit pane
+        self.current_edit_pane.setLayout(edit_layout)
+
+        # Add the edit pane to the main layout, to the right of the right pane
+        self.main_layout.addWidget(self.current_edit_pane)
+
+        # Show the edit pane
+        self.current_edit_pane.show()
 
 
 if __name__ == "__main__":
