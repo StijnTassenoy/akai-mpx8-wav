82002961c60af8059f0224e2b158194fba5cfd4a
StijnTassenoy <stijn.tassenoy@gmail.com>
2023-04-11 19:27:06 +0200

    Refactored and update readability

--- main.py
+++ main.py
@@ -13,12 +13,31 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.file_list = []
         self.soundeffect_list = []
 
         self.setWindowTitle("AKAI MPX8 Manager")
         self.setWindowIcon(QtGui.QIcon("images/logo.png"))
+        self._setup_left_pane()
+        self._setup_right_pane()
 
-        # Create the left pane for drag and drop
+        self._setup_pane_layout()
+
+        self.current_edit_pane = None
+        self._setup_edit_pane()
+
+
+        # Create a layout for the main window
+        self.main_layout = QVBoxLayout()
+        self.main_layout.addLayout(self.pane_layout)
+        self.main_layout.addWidget(self.edit_pane)
+
+        # Create a central widget and set the main layout
+        central_widget = QWidget()
+        central_widget.setLayout(self.main_layout)
+        self.setCentralWidget(central_widget)
+
+    def _setup_left_pane(self):
         self.left_pane = QLabel("Drag and drop files here")
         self.left_pane.setAcceptDrops(True)
         self.left_pane.setAlignment(Qt.AlignCenter)
@@ -27,26 +46,18 @@ class MainWindow(QMainWindow):
         self.left_pane.dragEnterEvent = self.dragEnterEvent
         self.left_pane.dropEvent = self.dropEvent
 
-        # Create the right pane for displaying file list
+    def _setup_right_pane(self):
         self.right_pane = QListView()
         self.right_pane.setSelectionMode(QAbstractItemView.SingleSelection)
         self.right_pane.setEditTriggers(QAbstractItemView.NoEditTriggers)
         self.right_pane.setStyleSheet(right_pane_style)
         self.right_pane.clicked.connect(self.show_edit_pane)
-
         # Create a model for the right pane
         self.model = QStandardItemModel(self.right_pane)
-
         # Set the model for the right pane
         self.right_pane.setModel(self.model)
 
-        # Create a layout for the panes
-        pane_layout = QHBoxLayout()
-        pane_layout.addWidget(self.left_pane)
-        pane_layout.addWidget(self.right_pane)
-
-        self.current_edit_pane = None
-
+    def _setup_edit_pane(self):
         # Create a layout for the edit pane
         edit_layout = QFormLayout()
         self.output_name_edit = QLineEdit()
@@ -59,18 +70,10 @@ class MainWindow(QMainWindow):
         self.edit_pane.setLayout(edit_layout)
         self.edit_pane.hide()
 
-        # Create a layout for the main window
-        self.main_layout = QVBoxLayout()
-        self.main_layout.addLayout(pane_layout)
-        self.main_layout.addWidget(self.edit_pane)
-
-        # Create a central widget and set the main layout
-        central_widget = QWidget()
-        central_widget.setLayout(self.main_layout)
-        self.setCentralWidget(central_widget)
-
-        # Initialize the file list
-        self.file_list = []
+    def _setup_pane_layout(self):
+        self.pane_layout = QHBoxLayout()
+        self.pane_layout.addWidget(self.left_pane)
+        self.pane_layout.addWidget(self.right_pane)
 
     def dragEnterEvent(self, event):
         if event.mimeData().hasUrls():
